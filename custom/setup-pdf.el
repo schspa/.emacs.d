d1cceda17ce8ca1a62a5018b5831f9c998dd2b34
schspa <schspa@gmail.com>
2019-10-22 22:30:53 +0800

    only setup pdf-tools & org-pdfview in GUI version
    Signed-off-by: schspa <schspa@gmail.com>

--- custom/setup-pdf.el
+++ custom/setup-pdf.el
@@ -1,15 +1,20 @@
-(use-package pdf-tools
-  :pin manual
-  :config
-  ;; iniitialize
-  (pdf-tools-install)
-  (setq-default pdf-view-display-size 'fit-page)
-  (setq pdf-annot-ctivate-create_annotations t)
-  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
+(when (display-graphic-p)
+  (progn
+	(use-package pdf-tools
+	  :pin manual
+	  :config
+	  ;; iniitialize
+	  (pdf-tools-install)
+	  (setq-default pdf-view-display-size 'fit-page)
+	  (setq pdf-annot-ctivate-create_annotations t)
+	  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
 
-(eval-after-load 'org '(require 'org-pdfview))
-(add-to-list 'org-file-apps
-			 '("\\.pdf\\'" . (lambda (file link)
-							   (org-pdfview-open link))))
+	(use-package org-pdfview
+	  :ensure t
+	  :config
+	  (eval-after-load 'org '(require 'org-pdfview))
+	  (add-to-list 'org-file-apps
+				   '("\\.pdf\\'" . (lambda (file link)
+									 (org-pdfview-open link)))))))
 
 (provide 'setup-pdf)
