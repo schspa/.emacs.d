8cd23aa9f2cac86083074060a71b91377475ac3b
schspa <schspa.gmail.com>
2019-11-13 14:44:44 +0800

    Switch to melpa office repository
    emacs china is lackof pdftools package
    
    Signed-off-by: schspa <schspa.gmail.com>

--- init.el
+++ init.el
@@ -1,10 +1,22 @@
 (when (>= emacs-major-version 24)
   (require 'package)
-  (package-initialize)
-  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-		                   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
-
-;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+  (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+					  (not (gnutls-available-p))))
+		 (proto (if no-ssl "http" "https")))
+	(when no-ssl
+	  (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+	;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+	(add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+	;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+	(when (< emacs-major-version 24)
+	  ;; For important compatibility libraries like cl-lib
+	  (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+  (package-initialize))
 
 ;; cl - Common Lisp Extension
 (require 'cl)
