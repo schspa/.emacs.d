059a29db153d888f8117a337cc198ba3751a81a6
schspa <schspa@gmail.com>
2020-01-19 17:37:42 +0800

    use index file to find posts rather than search in directory.
    Signed-off-by: schspa <schspa@gmail.com>

--- custom/setup-blog.el
+++ custom/setup-blog.el
@@ -32,15 +32,23 @@
   "Insert publish url"
   (replace-regexp-in-string "BLOG_PUBLISH_URL" "" from t))
 
+(defcustom schspa/blog-source-dir "~/Sync/org/"
+  "font-size in mm."
+  :group 'schspa
+  :type 'string)
+
+(defcustom schspa/blog-sites "~/sites/"
+  "font-size in mm."
+  :group 'schspa
+  :type 'string)
+
 (use-package org-static-blog
   :load-path (lambda () (expand-file-name "git/org-static-blog" user-emacs-directory))
   :ensure t)
 
 (setq org-static-blog-publish-title "schspa's Blog")
 (setq org-static-blog-publish-url (blog/fix-publish-url "BLOG_PUBLISH_URL/"))
-(setq org-static-blog-publish-directory "~/sites/")
-(setq org-static-blog-posts-directory "~/Sync/org/posts/")
-(setq org-static-blog-drafts-directory "~/sites/drafts/")
+(setq org-static-blog-publish-directory schspa/blog-sites)
 (setq org-static-blog-enable-tags t)
 (setq org-static-blog-use-preview t)
 (setq org-static-blog-preview-convert-titles t)
@@ -49,13 +57,12 @@
 
 (dolist (directory
 		 (list
-		  org-static-blog-publish-directory
-		  org-static-blog-posts-directory
-		  org-static-blog-drafts-directory))
+		  org-static-blog-publish-directory))
   (if (not (file-directory-p directory))
 	  (progn
 		(message "Create %S for org-static-blog" directory)
 		(make-directory directory))))
+
 ;; This header is inserted into the <head> section of every page:
 ;;   (you will need to create the style sheet at
 ;;    ~/projects/blog/static/style.css
@@ -81,6 +88,28 @@
   (concat (format-time-string "%Y/%m/%d" post-datetime)
           "/"
           (file-name-nondirectory post-filename)))
+
+(defun schspa/readlines (filePath)
+  "Return a list for a file"
+  (with-temp-buffer
+	(insert-file-contents filePath)
+	(split-string (buffer-string) "\n" t)))
+
+(defun schspa/getposts (filePath)
+  "Return posts file name"
+  (seq-filter 'file-readable-p
+			  (mapcar (lambda (arg)
+						(expand-file-name arg schspa/blog-source-dir))
+					  (schspa/readlines
+					   (expand-file-name filePath schspa/blog-source-dir)))))
+
+(defun org-static-blog-get-post-filenames ()
+  "Returns a list of all posts."
+  (schspa/getposts "posts"))
+
+(defun org-static-blog-get-draft-filenames ()
+  "Returns a list of all drafts."
+  (schspa/getposts "drafts"))
 
 (provide 'setup-blog)
 ;; coding: utf-8
