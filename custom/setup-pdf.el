144272deeb0609869610cd25c8b9a2d5942a6de7
schspa <schspa.gmail.com>
2020-04-13 21:22:00 +0800

    update pdfview settings
    Signed-off-by: schspa <schspa.gmail.com>

--- custom/setup-pdf.el
+++ custom/setup-pdf.el
@@ -1,19 +1,21 @@
-(when (display-graphic-p)
-  (progn
-	(use-package pdf-tools
-	  :config
-	  ;; iniitialize
-	  (pdf-tools-install)
-	  (setq-default pdf-view-display-size 'fit-page)
-	  (setq pdf-annot-ctivate-create_annotations t)
-	  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
+(use-package pdf-tools
+  :if (display-graphic-p)
+  :commands pdf-view-mode
+  :ensure t
+  :magic ("%PDF" . pdf-view-mode)
+  :config
+  (pdf-tools-install :no-query)
+  (setq-default pdf-view-display-size 'fit-page)
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
 
-	(use-package org-pdfview
-	  :ensure t
-	  :config
-	  (eval-after-load 'org '(require 'org-pdfview))
-	  (add-to-list 'org-file-apps
-				   '("\\.pdf\\'" . (lambda (file link)
-									 (org-pdfview-open link)))))))
+(use-package org-pdfview
+  :if (display-graphic-p)
+  :after (pdf-tools)
+  :ensure t
+  :config
+  (eval-after-load 'org '(require 'org-pdfview))
+  (add-to-list 'org-file-apps
+			   '("\\.pdf\\'" . (lambda (file link)
+								 (org-pdfview-open link)))))
 
 (provide 'setup-pdf)
