01f05a3d98b0bf33f5161fb4714e639c3a25563a
schspa <schspa@gmail.com>
2020-10-04 20:59:38 +0800

    add orgmode elpa
    Signed-off-by: schspa <schspa@gmail.com>

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-						   ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+						   ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+                           ("org" . "https://orgmode.org/elpa")))
   (package-initialize))
 
 ;; cl - Common Lisp Extension
