ce9f7c471620c6fa68c98660f91c479e446af0f2
schspa <schspa@gmail.com>
2020-10-09 22:17:04 +0800

    use org-gtd.
    Signed-off-by: schspa <schspa@gmail.com>

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -126,93 +126,59 @@
     (set-face-attribute 'org-table  nil  :font "Sarasa Mono SC")))
 
 ;; GTD
-;;set status for TODO.
-(setq org-todo-keywords
-      '((sequence "INBOX(t)" "ONGOING(o)" "LATER(l)" "WAIT/FORWARD(f)" "MAYBE/FUTURE(m)" "|" "CANCEL(c@)" "DONE(d)")))
-;; 配置状态颜色
-;; color for todo keywords
-(setq org-todo-keyword-faces '(
-                               ("INBOX" . (:foreground "blue" :weight bold))
-                               ("ONGOING" . org-warning)
-                               ("LATER" . "yellow")
-                               ("WAIT/FORWARD" . "blue")
-                               ("MAYBE/FUTURE" . "purple")
-                               ("DONE" . "green")
-                               ("CANCEL" . "grey")
-                               ))
+(use-package org-gtd
+  :ensure t
+  :after org
+  :demand t ;; without this, the package won't be loaded, so org-agenda won't be configured
+  :custom
+  ;; where org-gtd will put its files. This value is also the default one.
+  (org-gtd-directory "~/org/gtd/")
+  ;; package: https://github.com/Malabarba/org-agenda-property
+  ;; this is so you can see who an item was delegated to in the agenda
+  (org-agenda-property-list '("DELEGATED_TO"))
+  ;; I think this makes the agenda easier to read
+  (org-agenda-property-position 'next-line)
+  ;; package: https://www.nongnu.org/org-edna-el/
+  ;; org-edna is used to make sure that when a project task gets DONE,
+  ;; the next TODO is automatically changed to NEXT.
+  (org-edna-use-inheritance t)
+  (org-edna-load)
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c d c" . org-gtd-capture) ;; add item to inbox
+   ("C-c d a" . org-agenda-list) ;; see what's on your plate today
+   ("C-c d p" . org-gtd-process-inbox) ;; process entire inbox
+   ("C-c d n" . org-gtd-show-all-next) ;; see all NEXT items
+   ("C-c d s" . org-gtd-show-stuck-projects)) ;; see projects that don't have a NEXT item
+  :init
+  (bind-key "C-c c" 'org-gtd-clarify-finalize))
 
-;;add timestamp when todo change to done.
-(setq org-log-done 'time)
+(use-package org-agenda
+  :ensure nil ;; this is how you tell use-package to manage a sub-package
+  :after org-gtd ;; because we need to add the org-gtd directory to the agenda files
+  :custom
+  ;; use as-is if you don't have an existing org-agenda setup
+  ;; otherwise push the directory to the existing list
+  (org-agenda-files `(,org-gtd-directory))
+  ;; a useful view to see what can be accomplished today
+  (org-agenda-custom-commands '(("g" "Scheduled today and all NEXT items" ((agenda "" ((org-agenda-span 1))) (todo "NEXT"))))))
 
-;;Settings for global id link.
-(require 'org-id)
-(setq org-id-link-to-org-use-id t)
-
-(global-set-key "\C-cl" 'org-store-link)
-
-;;set priority
-(setq org-highest-priority ?A)
-(setq org-lowest-priority ?D)
-(setq org-default-priority ?A)
-(setq org-priority-faces '(
-                           (?A . org-warning)
-                           (?B . (:background "DodgerBlue" :foreground "black" :weight bold))
-                           (?C . (:foreground "SkyBlue" :weight bold))
-                           (?D . (:foreground "DodgerBlue" :weight bold))
-                           )
-      )
-
-
-;;Setting for gtd captures
-;;Directory for capture files.
-(setq org-directory "~/org/")
-(let ((dir (expand-file-name org-directory)))
-  (if dir (setq org-agenda-files (directory-files-recursively dir "org$"))))
-
-(global-set-key (kbd "C-c a") 'org-agenda)
-
-;;Default capture files.
-(setq org-default-notes-file (concat org-directory "gtd/inbox.org"))
-;;Capture template
-(setq org-capture-templates '(
-                              ("t" "Tasks" entry (file+headline "gtd/inbox.org" "Tasks") "* INBOX %?\nTime:%T\nFrom:%F\n")
-                              ("p" "Projects" entry (file+headline "gtd/inbox.org" "Projects")  "* %?\nTime:%U\nFrom:%F\n")
-                              ("i" "Item notes" item (file+headline "gtd/inbox.org" "Items")  "+ %?\n  Time:%U\n  From:%F\n")
-                              ("m" "Misc notes" plain (file+headline "misc.org" "Notes")  "-----------------------------------------------------\nTime:%U\n %?")
-                              )
-      )
-
-;;set tags
-;;where?(h/o/w),what?(c/l/s),when?(gtd|immeiately,wait,action),who?(my gtd,others),why?how?(delete/archieve/schedule)
-(setq org-tag-alist '(
-                      (:startgroup . nil)
-                      ("home" . ?r) ("office" . ?o) ("way" . ?w)
-                      (:endgroup . nil)
-                      ("职业" . ?c)
-                      ("生活" . ?l)
-                      ("学习" . ?s)
-                      ))
-
-;;targets for refile
-(setq org-refile-targets (quote (
-                                 (nil :maxlevel . 9)
-                                 (org-agenda-files :maxlevel . 9)
-                                 )
-                                )
-      )
-;;outline path for refile
-(setq org-refile-use-outline-path 'full-file-path)
-;;org-outline-path-complete-in-steps
-(setq org-outline-path-complete-in-steps t)
-;;create new parents while refile
-(setq org-refile-allow-creating-parent-nodes 'confirm)
-
-(setq org-outline-path-complete-in-steps nil)
-
-;;location for archive
-(setq org-archive-location (concat org-directory "gtd/_archive/" (format-time-string "%Y%m") "_archive.org::datetree/* Archive from %s"))
-;;information added to property when a subtree is moved
-(setq org-archive-save-context-info '(time file ltags itags todo category olpath))
+(use-package org-capture
+  :ensure nil
+  ;; note that org-gtd has to be loaded before this
+  :after org-gtd
+  :config
+  ;; use as-is if you don't have an existing set of org-capture templates
+  ;; otherwise add to existing setup
+  ;; you can of course change the letters, too
+  (setq org-capture-templates `(("i" "Inbox"
+                                 entry (file ,(org-gtd--path org-gtd-inbox-file-basename))
+                                 "* %?\n%U\n\n  %i"
+                                 :kill-buffer t)
+                                ("l" "Todo with link"
+                                 entry (file ,(org-gtd--path org-gtd-inbox-file-basename))
+                                 "* %?\n%U\n\n  %i\n  %a"
+                                 :kill-buffer t))))
 
 ;; latex settings
 (setq-default org-preview-latex-image-directory (concat my-cache-dir "/org-latex"))
@@ -287,6 +253,11 @@ org-protocol://gdt?id=CBEC8DD1-7814-44A7-AA3D-97AEC35B6DB7"
          :base-url "org/"
          :working-directory "~/org/")))
 
+(use-package ox-gfm
+  :ensure t
+  :config
+  (eval-after-load "org"
+    '(require 'ox-gfm nil t)))
 ;;set agenda files
 (provide 'setup-org)
 
