3268797567c0ba6e332471b05265288ac8bf78ae
schspa <schspa@gmail.com>
2021-02-28 17:11:56 +0800

    email: add work email setup with system keyring
    Signed-off-by: schspa <schspa@gmail.com>

--- custom/setup-mail.el
+++ custom/setup-mail.el
@@ -23,11 +23,65 @@
 
 ;;; Code:
 
+;; Set work email with fellowing elisp code.
+;; (secrets-create-item
+;;  (secrets-get-alias "default")
+;;  "work-mail" "password"
+;;  :service "email"
+;;  :user-full-name "Zhaohui Shi"
+;;  :username "zhaohui.shi"
+;;  :host "mail.domain.com"
+;;  :user-mail-address "zhaohui.shi@domain.com"
+;;  :maildir "^/work"
+;;  :get-mail-command "offlineimap -a work")
+
+(defun get-mail-conf (service attribute)
+  "Get email configuration for system keyring"
+  (let* (
+         (collect (secrets-get-alias "default"))
+         (item (car (secrets-search-items collect :service service))))
+    (if (equal attribute :password) (secrets-get-secret collect item)
+      (secrets-get-attribute collect item attribute))))
+
 (use-package mu4e
   :load-path "/usr/share/emacs/site-lisp/mu4e"
   :commands mu4e
   :config
-  (setq mu4e-get-mail-command "offlineimap"))
+  (setq mu4e-contexts
+        `( ,(make-mu4e-context
+	         :name "Private"
+	         :enter-func (lambda () (mu4e-message "Entering Private context"))
+             :leave-func (lambda () (mu4e-message "Leaving Private context"))
+	         ;; we match based on the maildir of the message
+             :match-func (lambda (msg)
+                           (when msg
+                             (mu4e-message-contact-field-matches msg
+                                                                 :maildir "^/gmail")))
+	         :vars '(( user-mail-address     . "schspa@gmail.com"  )
+		             ( user-full-name	     . "Schspa Shi" )
+                     ( mu4e-get-mail-command . "proxychains offlineimap -a gmail")))
+           ))
+  (when (get-mail-conf "email" :maildir)
+    (add-to-list 'mu4e-contexts
+                 (make-mu4e-context
+                  :name "work"
+                  :enter-func (lambda () (mu4e-message "Switch to the Work context"))
+                  ;; no leave-func
+                  ;; we match based on the maildir of the message
+                  :match-func (lambda (msg)
+                                (when msg
+                                  (mu4e-message-contact-field-matches
+                                   msg
+                                   :maildir (get-mail-conf "email" :maildir))))
+                  :vars
+                  `(( user-mail-address       . ,(get-mail-conf "email" :user-mail-address))
+                    ( user-full-name          . ,(get-mail-conf "email" :user-full-name))
+                    ( mu4e-compose-signature  . ,(concat
+                                                  (get-mail-conf "email" :user-full-name)
+                                                  "\n"
+                                                  "BRs\n"))
+                    ( mu4e-get-mail-command   . ,(get-mail-conf "email" :get-mail-command))))))
+  )
 
 (provide 'setup-mail)
 
